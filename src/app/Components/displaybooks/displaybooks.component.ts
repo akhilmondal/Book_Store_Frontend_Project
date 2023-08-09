13a74dbdc36e91598e8f682fa9e29fb8527f2163
akhilmondal <akhilmandalfamily@gmail.com>
2023-08-09 12:45:26 +0530

    [Akhil] Added: frontend for single book details when we click the book.

--- src/app/Components/displaybooks/displaybooks.component.ts
+++ src/app/Components/displaybooks/displaybooks.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-displaybooks',
   templateUrl: './displaybooks.component.html',
-  styleUrls: ['./displaybooks.component.scss']
+  styleUrls: ['./displaybooks.component.scss'],
 })
 export class DisplaybooksComponent {
+  constructor(private router: Router) {}
   @Input() books: any = [];
   @Input() bookCount: any;
 
-
+  goToBookDetails(bookId: string) {
+    this.router.navigateByUrl('/dashboard/getallbooks/' + bookId);
+  }
 }
