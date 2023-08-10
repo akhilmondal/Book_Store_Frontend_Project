b314b7d850e2ec4bc0c64e75ad7010b1da39e82d
akhilmondal <akhilmandalfamily@gmail.com>
2023-08-10 16:29:25 +0530

    [Akhil] Added: functionality to search books according to the book name, description and author.

--- src/app/Components/displaybooks/displaybooks.component.ts
+++ src/app/Components/displaybooks/displaybooks.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/Services/dataService/data.service';
 
 @Component({
   selector: 'app-displaybooks',
@@ -7,9 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./displaybooks.component.scss'],
 })
 export class DisplaybooksComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private data: DataService) {}
   @Input() books: any = [];
   @Input() bookCount: any;
+  searchMessage!: string;
+  messageData!: string;
+  subscription!: Subscription;
+  ngOnInit() {
+    this.subscription = this.data.currentSearchInput.subscribe(
+      (message) => (this.searchMessage = message)
+    );
+
+    // Search Data coming from dashboard to display using data service
+    this.data.currentSearchInput.subscribe((result: any) => {
+      this.searchMessage = result;
+      console.log(this.searchMessage);
+    });
+  }
 
   goToBookDetails(bookId: string) {
     this.router.navigateByUrl('/dashboard/getallbooks/' + bookId);
